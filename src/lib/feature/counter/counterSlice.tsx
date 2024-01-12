cac836228d3f24691fb6ef87de5011a2398b53db
Adimaryadi <adimaryaditech@gmail.com>
2024-01-12 21:46:46 +0700

    added file

---
+++ src/lib/feature/counter/counterSlice.tsx
@@ -0,0 +1,31 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+// Define the initial state
+interface CounterState {
+  count: number;
+}
+
+const initialState: CounterState = {
+  count: 0,
+};
+
+// Create a Redux slice
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.count += 1;
+    },
+    decrement: (state) => {
+      state.count -= 1;
+    },
+    initializeCount: (state, action: PayloadAction<number>) => {
+      state.count = action.payload;
+    },
+  },
+});
+
+// Export the actions and reducer
+export const { increment, decrement, initializeCount } = counterSlice.actions;
+export default counterSlice.reducer;
